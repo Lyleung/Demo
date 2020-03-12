69fc10ac40d47b566ddacefe6fc66cad56140b8a
samsonllam <fongpuilam@gmail.com>
2020-03-13 01:10:10 +0800

    add BoardModel

---
+++ Assets/Model/BoardModel.cs
@@ -0,0 +1,10 @@
+﻿public class BoardModel {
+
+    // 0: havnt played
+    // 1: player 1 won
+    // 2: player 2 won
+
+    static public int gameOne = 1;
+    static public int gameTwo = 1;
+    // More...
+}
