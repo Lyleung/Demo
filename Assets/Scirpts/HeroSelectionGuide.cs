42d292b94aa829bbf9e59950202f6ccf949e2ec3
kat <katherine.lee@invbots.com>
2020-04-10 13:12:26 +0800

    Selection guide and hero description added

---
+++ Assets/Scirpts/HeroSelectionGuide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroSelectionGuide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject guide;
+    // Start is called before the first frame update
+
+    IEnumerator RemoveAfterSeconds (int seconds, GameObject obj){
+        yield return new WaitForSeconds(5.0f);
+        obj.SetActive(false);
+    }
+
+    void Start()
+    {
+        StartCoroutine(RemoveAfterSeconds(5, guide));
+    }
+}
