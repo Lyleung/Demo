4707f6423c8d26b8d0b8a252cbfa4f2baa304416
kat <katherine.lee@invbots.com>
2020-04-10 13:19:08 +0800

    Selection guide fades away

--- Assets/Scirpts/HeroSelectionGuide.cs
+++ Assets/Scirpts/HeroSelectionGuide.cs
@@ -8,13 +8,27 @@ public class HeroSelectionGuide : MonoBehaviour
     public GameObject guide;
     // Start is called before the first frame update
 
-    IEnumerator RemoveAfterSeconds (int seconds, GameObject obj){
-        yield return new WaitForSeconds(5.0f);
-        obj.SetActive(false);
+    // IEnumerator RemoveAfterSeconds (int seconds, GameObject obj){
+    //     yield return new WaitForSeconds(5.0f);
+    //     obj.SetActive(false);
+    // }
+
+    IEnumerator Fade()
+    {
+        Renderer renderer = guide.GetComponent<Renderer>();
+        Color newColor = renderer.material.color;      
+        for (float f = 3f; f >= 0; f -= 0.1f)
+        {  
+            newColor.a = f;
+            renderer.material.color = newColor;
+            yield return new WaitForSeconds(.1f);
+        }
     }
+
 
     void Start()
     {
-        StartCoroutine(RemoveAfterSeconds(5, guide));
+        // StartCoroutine(RemoveAfterSeconds(5, guide));
+        StartCoroutine(Fade());
     }
 }
